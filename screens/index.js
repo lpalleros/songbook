23507ff86167fc0bc9e870bd9a8b1737941f203a
lucaspalleros <lucaspalleros@gmail.com>
2022-03-28 15:50:46 -0300

    screens setup

---
+++ screens/index.js
@@ -0,0 +1,3 @@
+export { default as HomeScreen } from './HomeScreen'
+export { default as SongListScreen } from './SongListScreen'
+export { default as SongDetailsScreen } from './SongDetailsScreen'
